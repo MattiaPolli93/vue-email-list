444650bc8b0a0dcd4468003f24f1610a5a246202
Mattia <matpolli93@gmail.com>
2021-04-07 15:37:16 +0200

    Complete exercise

--- js/script.js
+++ js/script.js
@@ -5,6 +5,13 @@
 const app = new Vue({
     el: "#app",
     data: {
-
+        list: []
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((result) => {
+                this.list.push(result.data.response);
+            });
+        }
     }
 });
